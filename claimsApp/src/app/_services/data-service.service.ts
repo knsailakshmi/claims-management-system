1daad3cc440cc5614d11163efa20171534045838
Prithvi <prithvisinghtewatia@gmail.com>
2022-07-09 14:13:38 +0530

    Completed view claim. Needs auth fix

--- claimsApp/src/app/_services/data-service.service.ts
+++ claimsApp/src/app/_services/data-service.service.ts
@@ -8,21 +8,33 @@ import {DatePipe} from "@angular/common";
 })
 export class DataService {
 
+  auth:string=""
+  httpHeaders=new HttpHeaders({
+    'Content-Type':'application/json',
+    'Cache-Control':'no-cache',
+    'Authorization':this.auth
+  })
   constructor(private http:HttpClient,private datePipe:DatePipe) { }
 
-  sendSubmitClaimRequest(auth: string,requestBody:ClaimRequest){
+  sendSubmitClaimRequest(auth:string,requestBody:ClaimRequest){
     let url="http://localhost:8000/claim"
-    let httpHeaders=new HttpHeaders({
-      'Content-Type':'application/json',
-      'Cache-Control':'no-cache',
-      'Authorization':auth
-    })
     this.datePipe.transform(requestBody.policyStartDate,"yyyy-MM-dd")
     this.datePipe.transform(requestBody.policyEndDate,"yyyy-MM-dd")
     console.log(requestBody)
     return this.http.post(url,requestBody,{
-      headers:httpHeaders,
+      headers:this.httpHeaders,
       observe:"response"
+    })
+  }
+
+  getClaimsByMember(auth: string | null, userId: string){
+    let url=`http://localhost:8000/claim/user/${userId}/view`
+    if (auth != null) {
+      this.auth = auth
+    }
+    return this.http.get(url,{
+      headers:this.httpHeaders,
+      observe:"response",
     })
   }
 }
--- claimsApp/src/app/claimsstatus/claimsstatus.component.ts
+++ claimsApp/src/app/claimsstatus/claimsstatus.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 import {Claim} from "../models/Claim";
+import {DataService} from "../_services/data-service.service";
+import {TokenStorageService} from "../_services/token-storage.service";
 
 @Component({
   selector: 'app-claimsstatus',
@@ -11,23 +13,21 @@ export class ClaimsstatusComponent implements OnInit {
 
   claims: Claim[] = []
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              private dataService: DataService,
+              private tokenStorageService: TokenStorageService) {
   }
 
   ngOnInit(): void {
-    let url = "http://localhost:8080/claim"
-    let httpHeaders = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Cache-Control': 'no-cache'
-    })
-    this.http.get(url, {
-      headers: httpHeaders,
-      observe: "response"
-    }).subscribe(response => {
-      // @ts-ignore
-      this.claims=response.body["content"]
-      console.log(this.claims[0])
-    })
+    if (this.tokenStorageService.getToken() !== null) {
+      this.dataService.getClaimsByMember(this.tokenStorageService.getToken(),
+        this.tokenStorageService.getUser().userid).subscribe(response => {
+        console.log(response)
+        // @ts-ignore
+        this.claims = response.body
+      })
+    }
+
   }
 
 }
