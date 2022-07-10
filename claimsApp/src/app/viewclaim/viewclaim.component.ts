280a4ff1064b45afc76cbd8b96e1c5167d287124
Prithvi <prithvisinghtewatia@gmail.com>
2022-07-10 12:43:29 +0530

    Created table for claims on admin side

--- claimsApp/src/app/viewclaim/viewclaim.component.ts
+++ claimsApp/src/app/viewclaim/viewclaim.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router'
+import {TokenStorageService} from "../_services/token-storage.service";
+import {DataService} from "../_services/data-service.service";
+import {Claim} from "../models/Claim";
 
 @Component({
   selector: 'app-viewclaim',
@@ -8,11 +11,24 @@ import {Router} from '@angular/router'
 })
 export class ViewclaimComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  claims:Claim[]=[]
+
+  constructor(private router: Router,
+              private tokenStorageService:TokenStorageService,
+              private dataService:DataService) { }
 
   ngOnInit(): void {
+    let token=this.tokenStorageService.getToken()
+    if (token != null) {
+      this.dataService.getAllPendingClaims()
+        .subscribe(response=>{
+          // @ts-ignore
+          this.claims=response.body
+        })
+    }
+
   }
-  onUpdate(){
+  onUpdate(claimId:number){
     this.router.navigateByUrl('processclaim');
   }
 
