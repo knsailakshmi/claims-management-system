67b20030b7b624df479c1c720f5e1309a5da976e
Prithvi <prithvisinghtewatia@gmail.com>
2022-07-10 12:38:13 +0530

    Fixed models

--- claimsApp/src/app/models/Claim.ts
+++ claimsApp/src/app/models/Claim.ts
@@ -1,9 +1,12 @@
-export  class Claim{
-  claimId!:number;
-  description!:string;
-  claimRaisedDate!:Date;
-  claimSettledDate!:Date|null;
-  claimAmount!:number;
-  claimStatus!:string;
-  remarks!:string
+import {Policy} from "./Policy";
+
+export  interface Claim{
+  claimId:number
+  description:string
+  claimRaisedDate:Date
+  claimSettledDate:Date|null
+  claimAmount:number
+  claimStatus:string
+  remarks:string
+  policy:Policy
 }
---
+++ claimsApp/src/app/models/Policy.ts
@@ -0,0 +1,8 @@
+export interface Policy{
+  policyId:number
+  policyName:string
+  policyProvider:string
+  PolicyStartDate:string,
+  PolicyEndDate:string,
+  policyDescription:string
+}
